30866c727eff1bf11854e0af0aa229d1aa976c18
Michael J.H. Taggart <mtaggarthawk@gmail.com>
2025-11-29 22:40:42 -0700

    semantic HTML, and properly working javascript.

---
+++ scripts/form.js
@@ -0,0 +1,38 @@
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
+
+const productNameElement = document.getElementById("product-name")
+function generateOptions(){
+    products.forEach((product) => {
+        let newOption = document.createElement("option")
+        newOption.setAttribute("value", product.id)
+        newOption.textContent = product.name
+        productNameElement.appendChild(newOption)
+    })
+}
+generateOptions()
